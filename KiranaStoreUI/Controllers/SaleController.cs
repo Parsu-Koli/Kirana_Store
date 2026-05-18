1029020986a7dde20d77bee495640e224f953ac9
Prashant Koli <koliprashant11112@gmail.com>
2026-05-18 18:34:50 +0530

    time getting error so i changed

--- KiranaStoreUI/Controllers/SaleController.cs
+++ KiranaStoreUI/Controllers/SaleController.cs
@@ -124,7 +124,7 @@ namespace KiranaStoreUI.Controllers
             // ✅ SET BILL TOTALS
             vm.Sale.TotalAmount = totalAmount;
             vm.Sale.NetAmount = totalAmount - vm.Sale.Discount;
-            vm.Sale.SaleDate = DateTime.Now;
+            vm.Sale.SaleDate = DateTime.UtcNow;
 
             // 3️⃣ Create Sale
             var saleResponse = await _client.PostAsJsonAsync("Sale/AddSale", vm.Sale);
