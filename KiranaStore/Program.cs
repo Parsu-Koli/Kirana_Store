e2f566d68ff432d7a85073fff5d8227a15b85ae2
Parsu-Koli <koliprashant11112@gmail.com>
2026-07-22 18:09:53 +0530

    Comment out DbContext configuration
    Comment out the database context configuration for PostgreSQL.

--- KiranaStore/Program.cs
+++ KiranaStore/Program.cs
@@ -67,9 +67,11 @@ namespace KiranaStore
 
 
             //Server Connection
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                      options.UseNpgsql(
-                 builder.Configuration.GetConnectionString("DefaultConnection")));
+            // builder.Services.AddDbContext<AppDbContext>(options =>
+            //           options.UseNpgsql(
+            //      builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Configuration.GetConnectionString("DefaultConnection")
 
             // Repositories
             builder.Services.AddScoped<IAuthRepository, AuthRepository>();
